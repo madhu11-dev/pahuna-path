0ab75d41388c5ea85e3cc74e8d90a18667c4f47c
Madhu Chaudhary <chaudharymadhu502@gmail.com>
2025-11-11 13:53:31 +0545

    register function added

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,30 +1,36 @@
-<?php 
+<?php
 
 namespace App\Http\Controllers;
 
 use App\Http\Requests\RegisterRequest;
+use App\Http\Resources\ErrorMessages\ErrorResource;
+use App\Http\Resources\UserResources\RegistrationResource;
 use App\Services\AuthService;
+use Throwable;
 
-class UserController extends Controller{
+class UserController extends Controller
+{
 
-    public function __construct(protected AuthService $authService){}
+    public function __construct(protected AuthService $authService) {}
 
-    function register(RegisterRequest $request){
+    function register(RegisterRequest $request)
+    {
         try {
 
-            [$user]= $this->authService->authRegister($request->name, $request->email,$request->password);
-            
+            [$user, $verificationUrl] = $this->authService->authRegister(
+                $request->name,
+                $request->email,
+                $request->password
+            );
 
-        } catch (\Throwable $th) {
-           
+            return (new RegistrationResource((object)[
+                'user'             => $user,
+                'verificationurl'  => $verificationUrl
+            ]))->response()->setStatusCode(200);
+        } catch (Throwable $e) {
+            return (new ErrorResource((object)[
+                'message' => $e->getMessage()
+            ]))->response()->setStatusCode(400);
         }
-
-
     }
-
-
-
-
-
-
 }
