0028e8b534a058eb5b5aa278186233805b189c5e
Madhu Chaudhary <chaudharymadhu502@gmail.com>
2025-11-11 13:14:01 +0545

    register function added

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,9 +9,16 @@ class UserController extends Controller{
 
     public function __construct(protected AuthService $authService){}
 
-    function register(RegisterRequest $requesr){
+    function register(RegisterRequest $request){
+        try {
 
-        
+            [$user]= $this->authService->authRegister($request->name, $request->email,$request->password);
+
+
+        } catch (\Throwable $th) {
+           
+        }
+
 
     }
 
