38d19c125968f84ce137e0739cd6b5727174f5ad
Madhu Chaudhary <chaudharymadhu502@gmail.com>
2025-11-12 10:58:41 +0545

    verify-email route added

--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,9 @@
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-// Route::prefix('auth')->group(function(){
+Route::prefix('auth')->group(function () {
 
-//     Route::post('/register', [UserController::class, 'register']);
-
-
-
-// });
-Route::post('/register', [UserController::class, 'register']);
+    Route::post('/register', [UserController::class, 'register']);
+    Route::get('/verify/{id}/{hash}', [UserController::class, 'verifyEmail'])
+        ->name('verification.verify');
+});
