1728a62aa27657d5e5122b52eaefd9c29e50814d
Madhu Chaudhary <chaudharymadhu502@gmail.com>
2025-11-14 13:39:07 +0545

    middleware created for admin validation and also group admin route created

---
+++ app/Http/Middleware/AuthAdmin.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+
+class AuthAdmin
+{
+
+    public function handle(Request $request, Closure $next)
+    {
+
+        if (Auth::check()) {
+
+            if (Auth::user()->utype === 'ADM') {
+                return $next($request);
+            } else {
+                Session::flash('error', 'You do have admin access');
+                return redirect()->route('login');
+            }
+        }
+        return redirect()->route('login');
+    }
+}
