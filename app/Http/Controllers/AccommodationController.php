7dfeb7e9682552e51db7d5bc57c1e87ff9dde7b7
Risesh Shrestha <riseshstha222@gmail.com>
2025-11-20 09:51:56 +0000

    Delete function created for accommodation

--- app/Http/Controllers/AccommodationController.php
+++ app/Http/Controllers/AccommodationController.php
@@ -18,6 +18,41 @@ class AccommodationController extends Controller
         return AccommodationResource::collection(Accommodation::latest()->get());
     }
 
+    public function destroy(Accommodation $accommodation)
+    {
+        if ($accommodation->images) {
+            foreach ($accommodation->images as $imageUrl) {
+                $parsedUrl = parse_url($imageUrl);
+                if (isset($parsedUrl['path'])) {
+                    $path = ltrim($parsedUrl['path'], '/');
+                    if (strpos($path, 'storage/') === 0) {
+                        $path = substr($path, 8);
+                    }
+                    if (Storage::disk('public')->exists($path)) {
+                        Storage::disk('public')->delete($path);
+                    }
+                }
+            }
+        }
+
+        $accommodation->delete();
+        return response()->json(['message' => 'Accommodation deleted successfully.']);
+    }
+
+    protected function buildPublicStorageUrl($request, string $path): string
+    {
+        $rawUrl = Storage::disk('public')->url($path);
+        $rawUrl = str_replace('/public/', '/', $rawUrl);
+
+        $baseUrl = $this->resolveBaseUrl($request);
+
+        if (Str::startsWith($rawUrl, ['http://', 'https://'])) {
+            return preg_replace('#^https?://[^/]+#', $baseUrl, $rawUrl);
+        }
+
+        return $baseUrl . '/' . ltrim($rawUrl, '/');
+    }
+
     protected function resolveBaseUrl($request): string
     {
         $baseUrl = rtrim(config('app.url') ?? '', '/');
