32b6b4fb4a4d40dcaea2c1ed5b944189a5e0da79
Risesh Shrestha <riseshstha222@gmail.com>
2025-11-20 09:13:29 +0000

    Base url resolver function created

--- app/Http/Controllers/AccommodationController.php
+++ app/Http/Controllers/AccommodationController.php
@@ -17,4 +17,26 @@ class AccommodationController extends Controller
     {
         return AccommodationResource::collection(Accommodation::latest()->get());
     }
+
+    protected function resolveBaseUrl($request): string
+    {
+        $baseUrl = rtrim(config('app.url') ?? '', '/');
+
+        if (!$baseUrl) {
+            $baseUrl = rtrim($request->getSchemeAndHttpHost() ?? '', '/');
+        }
+
+        if (!$baseUrl) {
+            $baseUrl = 'http://localhost';
+        }
+
+        $parsed = parse_url($baseUrl);
+        $port = $parsed['port'] ?? env('APP_PORT', 8090);
+
+        if (!isset($parsed['port']) && $port && !in_array((int) $port, [80, 443])) {
+            $baseUrl .= ':' . $port;
+        }
+
+        return $baseUrl;
+    }
 }
