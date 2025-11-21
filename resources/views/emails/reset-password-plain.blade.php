8ad02688a4b48b48b7647c2cd8dd1d606ad90786
Risesh Shrestha <riseshstha222@gmail.com>
2025-11-21 08:17:06 +0000

    Reset password url template

---
+++ resources/views/emails/reset-password-plain.blade.php
@@ -0,0 +1,11 @@
+Hello,
+
+You recently requested to reset your password for Pahuna Path. mic check.
+
+Please click the link below to choose a new password:
+
+{!! $url !!}
+
+If you did not request a password reset, no further action is required.
+
+Thank you!
