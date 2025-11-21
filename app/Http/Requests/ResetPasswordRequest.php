835b8a5a6d3d632d180491ea49b0856d603cc45e
Madhu Chaudhary <chaudharymadhu502@gmail.com>
2025-11-21 14:33:39 +0545

    error messages have been added for the response

--- app/Http/Requests/ResetPasswordRequest.php
+++ app/Http/Requests/ResetPasswordRequest.php
@@ -3,7 +3,9 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
-class ResetPasswordRequest extends FormRequest {
+
+class ResetPasswordRequest extends FormRequest
+{
     public function authorize(): bool
     {
         return true;
@@ -16,6 +18,20 @@ class ResetPasswordRequest extends FormRequest {
             'email' => ['required', 'email', 'exists:users,email'],
             'password' => ['required', 'string', 'min:6', 'confirmed'],
             'password_confirmation' => ['required', 'string', 'min:6'],
+        ];
+    }
+    public function messages(): array
+    {
+        return [
+            'token.required' => 'Reset token is required.',
+            'email.required' => 'Email is required.',
+            'email.email' => 'Please provide a valid email address.',
+            'email.exists' => 'We could not find a user with that email address.',
+            'password.required' => 'Password is required.',
+            'password.min' => 'Password must be at least 6 characters.',
+            'password.confirmed' => 'Passwords do not match.',
+            'password_confirmation.required' => 'Confirm Password is required.',
+            'password_confirmation.min' => 'Confirm Password must be at least 6 characters.',
         ];
     }
 }
