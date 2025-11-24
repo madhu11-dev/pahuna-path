c71c4f0880805bfdad5d2d2bd5e899a4f062d325
Madhu Chaudhary <chaudharymadhu502@gmail.com>
2025-11-24 14:10:36 +0545

    register user action added for the profile pic

--- app/Actions/AuthorizationActions/RegisterUserAction.php
+++ app/Actions/AuthorizationActions/RegisterUserAction.php
@@ -3,18 +3,31 @@
 namespace App\Actions\AuthorizationActions;
 
 use App\Models\User;
+use App\Services\FileUploadService;
 use Illuminate\Support\Facades\Hash;
 
 class RegisterUserAction
 {
+    public function __construct(protected FileUploadService $fileUploadService) {}
 
-    public function handle(array $validaor)
+    public function handle(array $validatedData)
     {
+        $profilePicturePath = null;
+        
+        // Handle profile picture upload if provided
+        if (isset($validatedData['profile_picture']) && $validatedData['profile_picture']) {
+            $profilePicturePath = $this->fileUploadService->uploadProfilePicture(
+                $validatedData['profile_picture']
+            );
+        }
+
         $user = User::create([
-            'name' => $validaor['name'],
-            'email' => $validaor['email'],
-            'password' => Hash::make($validaor['password']),
+            'name' => $validatedData['name'],
+            'email' => $validatedData['email'],
+            'password' => Hash::make($validatedData['password']),
+            'profile_picture' => $profilePicturePath,
         ]);
+        
         $user->sendEmailVerificationNotification();
 
         return $user;
